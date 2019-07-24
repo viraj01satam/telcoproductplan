fd1ebecbba67ea307f10542b4b7181eb5c21ad9b
gowthamrajamohan <GX00571569@techmahindra.com>
2019-07-24 16:58:48 +0530

    Test Commit

--- VF-Product-Plan/src/main/java/com/vodafone/main/VfProductPlanApplication.java
+++ VF-Product-Plan/src/main/java/com/vodafone/main/VfProductPlanApplication.java
@@ -3,7 +3,7 @@ package com.vodafone.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+//Test
 @SpringBootApplication(scanBasePackages= {"com.vodafone"})
 public class VfProductPlanApplication extends SpringBootServletInitializer {
 
